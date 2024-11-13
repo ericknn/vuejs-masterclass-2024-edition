ef668e978360b792b5a18cf780f7697a2a69e3dd
Erick Núñez <ericknn@gmail.com>
2024-11-13 17:10:22 -0600

    cambios

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,23 @@
+import { h } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: '/', name: 'home', component: HomeView },
+    { path: '/projects', name: 'projects', component: () => import('@/views/ProjectsView.vue') },
+    {
+      path: '/projects/:id',
+      name: 'single-project',
+      component: () => import('@/views/SingleProjectsViiew.vue'),
+    },
+    {
+      path: '/:catchAll(.*)*',
+      name: 'NotFound',
+      component: h('p', { style: 'color:red;' }, '404 Not Found'),
+    },
+  ],
 })
 
 export default router
