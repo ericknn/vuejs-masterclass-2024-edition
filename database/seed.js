fd228a8a10f6a3dc0cc35dc92980a6ae4ddf4cd0
Erick Núñez <ericknn@gmail.com>
2024-11-19 10:56:37 -0600

    Seeding fakerjs

--- database/seed.js
+++ database/seed.js
@@ -5,4 +5,17 @@ import { faker } from '@faker-js/faker'
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
 
-console.log(supabase)
+const seedProjects = async () => {
+  const name = faker.lorem.words(3)
+
+  const { data, error, status } = await supabase.from('projects').insert({
+    name: name,
+    slug: name.toLocaleLowerCase().replace(/ /g, '-'),
+    status: faker.helpers.arrayElement(['in-progress', 'completed']),
+    collaborators: faker.helpers.arrayElement[(1, 2, 3)],
+  })
+
+  console.log(data, status, error)
+}
+
+await seedProjects()
