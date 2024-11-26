39e2aba794e98b05f8864303ca145483acc33f3e
Erick Núñez <ericknn@gmail.com>
2024-11-26 12:17:29 -0600

    Refactoring The Project 2

---
+++ src/utils/supaQueries.ts
@@ -0,0 +1,15 @@
+import { supabase } from '@/lib/supabaseClient'
+import type { QueryData } from '@supabase/supabase-js'
+
+export const projectsQuery = supabase.from('projects').select()
+
+export type Projects = QueryData<typeof projectsQuery>
+
+export const projectQuery = (slug: string) =>
+  supabase.from('projects').select('*,tasks(id,name, status, due_date)').eq('slug', slug).single()
+
+export type Project = QueryData<ReturnType<typeof projectQuery>>
+
+export const taskWithProjectsQuery = supabase.from('tasks').select('*, projects(id,name,slug)')
+
+export type TasksWithProjects = QueryData<typeof taskWithProjectsQuery>
